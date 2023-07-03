56e76c77f0d211efbe9e9a36db9c52372faf4328
PossumCodes <117069636+PossumCodes@users.noreply.github.com>
2023-07-03 13:28:44 +1000

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lana and is{" "}
+        <a
+          href="https://github.com/PossumCodes/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
