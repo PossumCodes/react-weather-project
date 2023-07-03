7e52e0a82dbc66f7f39286e99180263a1b3b74d5
PossumCodes <117069636+PossumCodes@users.noreply.github.com>
2023-07-03 15:24:21 +1000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lana and is{" "}
-        <a
-          href="https://github.com/PossumCodes/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Lana and is{" "}
+          <a
+            href="https://github.com/PossumCodes/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
