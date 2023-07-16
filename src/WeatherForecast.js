4654418fe137ada0a2a5a5e55c00c026167ee64c
PossumCodes <117069636+PossumCodes@users.noreply.github.com>
2023-07-16 17:17:53 +1000

    added use effect to fix forecast bug and removed fahrenheit conversion from app

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -7,25 +7,34 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [forecastReady, setForecastReady] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setForecastReady(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setForecastReady(true);
   }
 
-  console.log(props);
-
   if (forecastReady) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "667d9f573c8af4c33457be5d561a9148";
+    let apiKey = "1dbf926d3b4417bf379db7043bec1047";
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,41 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("celsius");
-  function fahrenheitConvert(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function celsiusConvert(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span className="WeatherTemp">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={fahrenheitConvert}>
-            °F
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <span className="WeatherTemp">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={celsiusConvert}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemp">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </span>
+  );
 }
